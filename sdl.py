b0138f9fcf234bebdf787c783995080f0e2c770f
shmo <evgeny.a.naumov@gmail.com>
2016-09-02 14:14:56 -0400

    added sdl.py, util.py

---
+++ sdl.py
@@ -0,0 +1,13 @@
+import sdl2 as sd2
+import sdl2.ext as sdx
+
+
+def mk_sdl_surface(w, h, depth=32):
+    sp = sd2.SDL_CreateRGBSurface(0, w, h, depth,
+                                  0x000000ff, 0x0000ff00,
+                                  0x00ff0000, 0xff000000)
+    if not sp:
+        print(sd2.SDL_GetError())
+        raise ValueError('null pointer to surface returned')
+    else:
+        return sp.contents
---
+++ util.py
@@ -0,0 +1,41 @@
+import time
+
+
+def fx(f, x):
+    """ function takes a callable f and argument to said callable x
+        and returns f(x)
+    """
+    return f(x)
+
+
+class Timer():
+    def __init__(self):
+        self.mark = None
+
+    def set(self):
+        self.mark = time.time()
+
+    def now(self):
+        return time.time()
+
+    def elapsed(self):
+        if self.mark is None:
+            raise ValueError("mark not set")
+        return time.time() - self.mark()
+
+    def lap(self):
+        if self.mark is None:
+            raise ValueError("mark not set")
+        t = time.time()
+        d = t - self.mark
+        self.mark = t
+        return d
+
+    def clear(self):
+        self.mark = None
+
+    def sleep(self, t):
+        time.sleep(t)
+
+    def wait(self, until):
+        time.sleep(until - time.time())
